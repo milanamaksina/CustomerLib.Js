4a4fc10dad65b81494a55a7a08d429ee1a237005
Milana Maksina <m_maksinaa@mail.ru>
2022-09-12 22:37:52 +0300

    feat: add addres validator

--- scr/Entities/Constants.js
+++ scr/Entities/Constants.js
@@ -7,6 +7,19 @@ const constants = {
     invalidEmailError : 'invalid email', 
     notesIsEmptyError : 'notes should have at list one note', 
     lastPurchaseDateIsLowError : 'date cannot be lower than 2020-1-1',
+
+    addressLineIsEmptyError : 'addressLine is empty', 
+    addressLineLongError : 'addressLine is more than 100 letters', 
+    addressLine2LongError : 'addressLine2 is more than 100 letters', 
+    addressTypeWrongTypeError : 'addresstype is wrong', 
+    cityIsEmptyError : 'city is empty', 
+    cityLongError : 'city has more than 50 letters', 
+    postalCodeIsEmptyError : 'postal code is empty', 
+    postalCodeLengthError : 'postal code is more than 6 symbols', 
+    stateIsEmptyError : 'state is empty', 
+    stateLongError: 'state is too long', 
+    countryIsEmptyError: 'country is empty error', 
+    countryNameError: 'invalid country name'
 }
 
 module.exports.constants = {constants};
--- scr/Entities/Validators/AddressValidator.js
+++ scr/Entities/Validators/AddressValidator.js
@@ -8,35 +8,38 @@ module.exports.AddressValidator = class AddressValidator {
         if (!address) return result;
 
         if (!address.addressLine || address.addressLine.length == 0) {
-            result.push(constants.);
+            result.push(constants.addressLineIsEmptyError);
         }
         else if (address.addressLine.length > 100) {
-            result.push(constants.);
+            result.push(constants.addressLineLongError);
+        }
+        if (address.addressLine2.length > 100) {
+            result.push(constants.addressLine2LongError);
         }
         if (!(address.addressType === "Shipping" || address.addressType === "Billing")) {
-            result.push(constants.);
+            result.push(constants.addressTypeWrongTypeError);
         }
         if (!address.city || address.city.length == 0) {
-            result.push(constants.);
+            result.push(constants.cityIsEmptyError);
         } else if (address.city.length > 50) {
-            result.push(constants.);
+            result.push(constants.cityLongError);
         }
         if (!address.postalCode || address.postalCode.length == 0) {
-            result.push(constants.);
+            result.push(constants.postalCodeIsEmptyError);
         } else if (address.postalCode.length > 6) {
-            result.push(constants.);
+            result.push(constants.postalCodeLengthError);
         }
       
         if (!address.state || address.state.length == 0) {
-            result.push(constants.);
+            result.push(constants.stateIsEmptyError);
         } else if (address.state.length > 20) {
-            result.push(constants.);
+            result.push(constants.stateLongError);
         }
       
         if (!address.country || address.country.length == 0) {
-            result.push(constants.);
+            result.push(constants.countryIsEmptyError);
         } else if (!(address.country === "United States" || address.country === "Canada")) {
-            result.push(constants.);
+            result.push(constants.countryNameError);
         }
         
         return result;
